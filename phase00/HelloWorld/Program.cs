bfea90fe1654a95f075cf35139af1d3e67544bd2
Navid Nasyry <navidnasyry@gmail.com>
2023-09-29 20:59:23 +0330

    init: start phase00 for starting using c#

---
+++ phase00/HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MyFirstNamespace.Program.SayHello();
+Program2.SayHello();
+
+
+
+
+public class Program2
+{
+    public static void SayHello()
+    {
+        Console.Write("hello again and again...\n");
+        return; 
+    }
+    
+}
+
+namespace MyFirstNamespace
+{
+    class Program
+    {
+        public static void SayHello()
+        {
+            Console.Write("hello again...\n");
+            return;
+        }
+        
+    }
+    
+}
+
+
+
