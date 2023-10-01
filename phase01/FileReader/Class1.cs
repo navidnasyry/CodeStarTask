8e947a9bd607a16b491795be517a41a5be5e4681
Navid Nasyry <navidnasyry@gmail.com>
2023-10-01 22:47:48 +0330

    feat: added FileReader class for open folder and read context of the files

---
+++ phase01/FileReader/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace FileReader;
+public class MyFolder
+{
+
+
+
+    private string folderPath;
+    // private Dictionary<string, string> folderContent = new Dictionary<string, string>();
+    private List<string> fileName = new List<string>();
+    private List<string> fileContext = new List<string>();
+    
+
+    public MyFolder(string folderPath)
+    {
+        this.folderPath = folderPath;
+
+        foreach (string file in Directory.EnumerateFiles(folderPath, "*"))
+        {
+            string contents = File.ReadAllText(file);
+            // Console.WriteLine(contents);
+            // Console.WriteLine(file);
+            this.fileName.Add(file);
+            this.fileContext.Add(contents);
+        }
+
+    }
+
+
+    public List<string> GetFileName(){
+        return this.fileName;
+    }
+
+    public List<string> GetFileContext(){
+        return this.fileContext;
+    }
+    public string GetFolderPath(){
+        return this.folderPath;
+    }
+}
--- phase01/FullTextSearcher/Program.cs
+++ phase01/FullTextSearcher/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.IO;
+using FileReader;
+
+
+
+Console.WriteLine("Start Program...");
+
+Console.WriteLine("Enter Your Folder Path : ");
+
+string folderPath = Console.ReadLine();
+
+MyFolder myData = new MyFolder(folderPath);
+
+Console.WriteLine(myData.GetFolderPath());
+
+
+
+
+
+
+
+// for(int i=0 ; i<5 ; i++)
+// {
+//     Console.WriteLine("LOOP:");
+//     Console.WriteLine(myData.GetFileName());
+//     Console.WriteLine(myData.GetFileName()[i]);
+//     Console.WriteLine(myData.GetFileContext()[i]);
+// }
+
+
+// string getFolderPath(){
+
+//     string folderPath = Console.ReadLine();
+
+//     return folderPath;
+// }
+
