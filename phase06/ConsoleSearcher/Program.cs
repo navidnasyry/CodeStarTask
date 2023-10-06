062cf9081c712931f957387d1e3aa5551d324861
Navid Nasyry <navidnasyry@gmail.com>
2023-10-06 14:57:36 +0330

    init: start phase 06

---
+++ phase06/ConsoleSearcher/Program.cs
@@ -0,0 +1,21 @@
+﻿// using TextSearcher;
+
+
+
+if (args.Length == 2)
+{
+    RunInvertedIndexSearch(
+        args[0],
+        args[1]
+    );
+}
+else
+{
+    Console.WriteLine("Error: please Enter 2 parameter");
+    Console.WriteLine("Usage: run --project ConsoleSearcher.csproj <FolderPath> <Term>");
+    Console.WriteLine("\n<FolderPath> : all files in this folder. enter this folder path.");
+    Console.WriteLine("\n<Term> : enter term you want to search it");
+
+
+}
+
