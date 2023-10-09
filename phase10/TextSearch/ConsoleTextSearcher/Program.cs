a82845769b71a600c490f9d790b3d175bb0c67ff
Navid Nasyry <navidnasyry@gmail.com>
2023-10-09 16:39:48 +0330

    fix: update version of elasticsearch and rollback to pre version

---
+++ phase10/TextSearch/ConsoleTextSearcher/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Nest;
+using ConsoleTextSearcher;
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+IElasticClient client = ElasticClientFactory.CreateElasticClient();
+
+
+Console.WriteLine(ElasticClientFactory.CheckClientConnection());
